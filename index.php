83d59ca6452e03f9879a048d1b00d477682f1f78
Frédéric NOËL <contact@fredericnoel.com>
2022-02-07 15:13:01 +0100

    Répétition de sous-chaînes

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 // Expressions rationnelles
 
-$texte = "abcddddde";
-$recherche = "/abcd{3,5}e/"; // Nombre d'occurences de la lettre 'd' délimité de 3 à 5 occurences
-// $recherche = "/abcd*e/"; // Nombre d'occurences de la lettre 'd' de 0 à n fois
-// $recherche = "/abcd+e/"; // Nombre d'occurences de la lettre 'd' de 1 à n fois
-// $recherche = "/abcd?e/"; // Nombre d'occurences de la lettre 'd' de 0 à 1 fois
+$texte = "123456454545454565";
+$recherche = "/(45){3,5}/"; // 
 preg_match($recherche, $texte); // Renvoie 1
