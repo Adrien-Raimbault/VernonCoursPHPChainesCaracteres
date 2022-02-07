3d8f6c1168bc56f521abe2460abd289d0e9315f7
Frédéric NOËL <contact@fredericnoel.com>
2022-02-07 15:11:06 +0100

    Exemples de recherche avec syntaxe réduite

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 // Expressions rationnelles
 
-$texte = "abcddde";
-$recherche = "/d{3}/"; // Nombre d'occurences fixes
+$texte = "abcddddde";
+$recherche = "/abcd{3,5}e/"; // Nombre d'occurences de la lettre 'd' délimité de 3 à 5 occurences
+// $recherche = "/abcd*e/"; // Nombre d'occurences de la lettre 'd' de 0 à n fois
+// $recherche = "/abcd+e/"; // Nombre d'occurences de la lettre 'd' de 1 à n fois
+// $recherche = "/abcd?e/"; // Nombre d'occurences de la lettre 'd' de 0 à 1 fois
 preg_match($recherche, $texte); // Renvoie 1
