edd35eb2f70d3a974c81a208c82de566513bc653
Frédéric NOËL <contact@fredericnoel.com>
2022-02-07 12:23:19 +0100

    Réglage des variables locales avec setlocale()

--- index.php
+++ index.php
@@ -5,3 +5,7 @@ htmlspecialchars("Tom & Jerry");
 
 $html = '<h1><a href="index.php">Page index</a></h1>';
 echo "Texte : " . strip_tags($html, '<a>') . "<br />"; // 2e paramètres : tags autorisés
+
+// Conventions d'affichage locales
+// setlocale(catégorie, localité);
+setlocale(LC_ALL, 'fr_FR', 'fr', 'FR', 'fr_FR@euro');
