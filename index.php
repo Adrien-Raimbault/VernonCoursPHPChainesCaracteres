0a16abfab8bcdafb78f5c849e4b64bfe9beeb9df
Frédéric NOËL <contact@fredericnoel.com>
2022-02-07 15:00:38 +0100

    Nombre d'occurences fixes

--- index.php
+++ index.php
@@ -1,38 +1,6 @@
 <?php
 // Expressions rationnelles
 
-$texte = "J'ai adopté un chat"; 
-
-if (preg_match('/un chat|un chien/', $texte)) {
-    echo "La phrase contient l'expression 'un chat' ou 'un chien'";
-}
-else {
-    echo "La phrase ne contient ni 'un chat', ni 'un chien'";
-}
-
-$recherche = "/J'ai adopté un (chat|chien)/";
-preg_match($recherche, $texte); // Renvoie la valeur 1
-
-$texte2 = 'Les enfants sont rentrés à 12h45';
-// $recherche2 = '/[012][0123456789]h[012345][0123456789]/';
-$recherche2 = '/[0-2][0-9]h[0-5][0-9]/';
-// $recherche2 = '/[^3-9][0-9]h[0-5][0-9]/'; Caractères "interdits"
-echo preg_match($recherche2, $texte2); // Renvoie 1
-
-/*
-[[:lower:]] lettres minuscules
-[[:upper:]] lettre majuscules
-[[:alpha:]] caractères alphabétiques
-[[:digit:]] chiffres décimaux 
-[[:alnum:]] alphanumérique
-[[:xdigit:]] chiffres héxadécimaux
-[[:ascii:]] ASCII 0 à 127
-[[:blank:]] espace ou tabulatio
-
-[[:^alpha:]] tout sauf les caractères alphanumériques 
-
-\d chiffre décimal
-\w caractère constituant un mot
-\s espace
-*/
-
+$texte = "abcddde";
+$recherche = "/d{3}/"; // Nombre d'occurences fixes
+preg_match($recherche, $texte); // Renvoie 1
